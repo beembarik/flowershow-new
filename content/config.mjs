eb8e49e6d4a4b234bad6dfbc5fc731dde13a15e3
chocobaryummy <117326541+chocobaryummy@users.noreply.github.com>
2023-11-01 14:39:42 +0700

    Update config.mjs

--- content/config.mjs
+++ content/config.mjs
@@ -6,11 +6,9 @@ const config = {
   domain: "https://flowershow-new.vercel.app/",
   // links to the pages you want to link to in the navbar and in the footer
   navLinks: [{ href: "/blog", name: "Blog" }],
-   ...
   search: {
-    provider: "kbar",
+  provider: "kbar",
   },
-  ...
 };
 
 export default config;
