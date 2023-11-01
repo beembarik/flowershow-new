28ecdf71289d751e292551f3af5a30e881aa0e34
chocobaryummy <117326541+chocobaryummy@users.noreply.github.com>
2023-11-01 09:53:38 +0700

    Update config.mjs

--- content/config.mjs
+++ content/config.mjs
@@ -1,1 +1,18 @@
+const config = {
+  title: "Fawaid Tahsin",
+  description: "Blog ini berisi catatan selama mengikuti tahsin di RQA",
+  author: "Barik",
+  logo: "/images/logo.svg",
+  domain: "https://fawaidtahsin.vercel.app/",
+  // links to the pages you want to link to in the navbar and in the footer
+  navLinks: [{ href: "/blog", name: "Blog" }],
+};
+
+const userConfig = {
+  ...
+  navbarTitle: {
+    text: "Fawaid Tahsin",
+    logo: "/images/logo.svg", // optional
+  },
+};
 export default {};
